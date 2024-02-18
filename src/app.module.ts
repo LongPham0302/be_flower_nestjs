7eee1bf050eebf33015cd1f45b1c3976662f092d
LongPham0302 <bekeo0302@gmail.com>
2024-02-18 14:12:44 +0700

    add

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/user.entity';
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'flower_shop',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
